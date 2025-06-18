2dcb9812295a56a1056a889a899913e12e251561
Pietro Guerra <pietro.guerra@etec.sp.gov.br>
2025-06-18 01:23:55 -0300

    Single JavaScripr object - exercise 1.3-1.5

--- part1/exercise-app/src/App.jsx
+++ part1/exercise-app/src/App.jsx
@@ -16,8 +16,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  const parts = props.parts
-  const [part1, part2, part3] = parts
+  const { parts } = props
 
   return (
     <>
@@ -29,35 +28,39 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  const { parts } = props
+
   return (
-    <p>Number of exercises: {props.exercises}</p>
+    <p>Number of exercises: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State od a component',
-    exercises: 14
+  const course = { 
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
   return (
     <div>
-      <Header course={course}/>
+      <Header course={course.name}/>
 
-      <Content 
-        parts={[part1, part2, part3]}
-      />
+      <Content parts={course.parts}/>
 
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
